b632ce60720db0f0aef884919c671dea51f4a17e
Timothy Adamcik <tadamcik0425@gmail.com>
2022-08-18 11:51:00 -0400

    added custom player files

---
+++ TimTestVideoPlayer-iOS/Players/Custom/TACustomizedPlayerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TACustomizedPlayerViewController.swift
+//  TimTestVideoPlayer-iOS
+//
+//  Created by Timothy Adamcik on 6/21/22.
+//
+
+import Foundation
+import AVFoundation
+import AVKit
+
+class TACustomizedPlayerViewController: UIViewController {
+    
+}
+
---
+++ TimTestVideoPlayer-tvOS/Players/Custom/TACustomizedPlayerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TACustomizedPlayerViewController.swift
+//  TimTestVideoPlayer-tvOS
+//
+//  Created by Timothy Adamcik on 6/21/22.
+//
+
+import Foundation
+import AVFoundation
+import AVKit
+
+class TACustomizedPlayerViewController: UIViewController {
+    
+}
